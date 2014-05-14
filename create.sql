997f54df50d2c3b8cceec8bb2ba94d58e0cbf527
sarzwest <jiratom@seznam.cz>
2014-05-14 14:36:52 +0200

    vytvoreny sktipty pro tvorbu databaze, naplneni testovacimi daty a smazani databaze

--- create.sql
+++ create.sql
@@ -1,26 +1,14 @@
--- remove relationships
-ALTER TABLE Recept DROP FOREIGN KEY FKRecept63372;
-ALTER TABLE Surovina_Prodejna DROP FOREIGN KEY FKSurovina_P759736;
-ALTER TABLE Surovina_Prodejna DROP FOREIGN KEY FKSurovina_P295292;
-ALTER TABLE Surovina DROP FOREIGN KEY FKSurovina493040;
-ALTER TABLE Recept_Ingredience DROP FOREIGN KEY FKRecept_Ing442947;
-ALTER TABLE Recept_Ingredience DROP FOREIGN KEY FKRecept_Ing999432;
--- remove tables
-DROP TABLE IF EXISTS Ingredience;
-DROP TABLE IF EXISTS Surovina_Prodejna;
-DROP TABLE IF EXISTS Prodejna;
-DROP TABLE IF EXISTS Surovina;
-DROP TABLE IF EXISTS Recept;
-DROP TABLE IF EXISTS Autor;
-DROP TABLE IF EXISTS Recept_Ingredience;
+-- Spusteni skriptu z bashe
+-- mysql -D ${OSD_DB} -u ${OSD_USERNAME} -p${OSD_PASSWORD} < create.sql
+
 -- create tables
-CREATE TABLE Ingredience (id int(10) NOT NULL AUTO_INCREMENT, název_ingredience varchar(20), Receptid int(10) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB;
+CREATE TABLE Ingredience (id int(10) NOT NULL AUTO_INCREMENT, název_ingredience varchar(20), PRIMARY KEY (id)) ENGINE=InnoDB;
 CREATE TABLE Surovina_Prodejna (id int(10) NOT NULL, Surovinaid int(10) NOT NULL, Prodejnaid int(10) NOT NULL, PRIMARY KEY (id, Surovinaid, Prodejnaid)) ENGINE=InnoDB;
 CREATE TABLE Prodejna (místo_nákupu varchar(20), id int(10) NOT NULL AUTO_INCREMENT, PRIMARY KEY (id)) ENGINE=InnoDB;
 CREATE TABLE Surovina (id int(10) NOT NULL AUTO_INCREMENT, kusuVLednici int(10), trvanlivost date, Ingredienceid int(10) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB;
 CREATE TABLE Recept (id int(10) NOT NULL AUTO_INCREMENT, jméno_pokrmu varchar(20), Autorid int(10) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB;
 CREATE TABLE Autor (id int(10) NOT NULL AUTO_INCREMENT, autor_jmeno varchar(20), autor_prijmeni varchar(20), PRIMARY KEY (id)) ENGINE=InnoDB;
-CREATE TABLE Recept_Ingredience (id int(10) NOT NULL, Receptid int(10) NOT NULL, Ingredienceid int(10) NOT NULL, PRIMARY KEY (id, Receptid, Ingredienceid)) ENGINE=InnoDB;
+CREATE TABLE Recept_Ingredience (id int(10) NOT NULL, potrebaKusu int(10), Receptid int(10) NOT NULL, Ingredienceid int(10) NOT NULL, PRIMARY KEY (id, Receptid, Ingredienceid)) ENGINE=InnoDB;
 -- create foreign keys and constraints
 ALTER TABLE Recept ADD INDEX FKRecept63372 (Autorid), ADD CONSTRAINT FKRecept63372 FOREIGN KEY (Autorid) REFERENCES Autor (id);
 ALTER TABLE Surovina_Prodejna ADD INDEX FKSurovina_P759736 (Surovinaid), ADD CONSTRAINT FKSurovina_P759736 FOREIGN KEY (Surovinaid) REFERENCES Surovina (id);
--- insert.sql
+++ insert.sql
@@ -1,15 +1,23 @@
+-- Spusteni skriptu z bashe
+-- mysql -D ${OSD_DB} -u ${OSD_USERNAME} -p${OSD_PASSWORD} < insert.sql
+
 -- templates
+-- INSERT INTO Prodejna(id, místo_nákupu) VALUES (?, ?);
 -- INSERT INTO Autor(id, autor_jmeno, autor_prijmeni) VALUES (?, ?, ?);
 -- INSERT INTO Recept(id, jméno_pokrmu, Autorid) VALUES (?, ?, ?);
+-- INSERT INTO Ingredience(id, název_ingredience) VALUES (?, ?);
+-- INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (?, ?, ?, ?);
 -- INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (?, ?, ?, ?);
--- INSERT INTO Prodejna(id, místo_nákupu) VALUES (?, ?);
 -- INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (?, ?, ?);
--- INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (?, ?, ?);
--- INSERT INTO Recept_Ingredience(id, Receptid, Ingredienceid) VALUES (?, ?, ?);
 
 -- Prodejny
 INSERT INTO Prodejna(id, místo_nákupu) VALUES (1, "Kaufland");
 INSERT INTO Prodejna(id, místo_nákupu) VALUES (2, "Vietnamec na rohu");
+
+-- Autori receptu
+INSERT INTO Autor(id, autor_jmeno, autor_prijmeni) VALUES (1, "Tomas", "Jiricek");
+INSERT INTO Autor(id, autor_jmeno, autor_prijmeni) VALUES (2, "Jan", "Jakes");
+INSERT INTO Autor(id, autor_jmeno, autor_prijmeni) VALUES (3, "Zdenek", "Pohlreich");
 
 -- Recepty
 INSERT INTO Recept(id, jméno_pokrmu, Autorid) VALUES (1, "Topinky", 1);
@@ -17,21 +25,78 @@ INSERT INTO Recept(id, jméno_pokrmu, Autorid) VALUES (2, "Palacinky", 2);
 INSERT INTO Recept(id, jméno_pokrmu, Autorid) VALUES (3, "Tatarak", 1);
 INSERT INTO Recept(id, jméno_pokrmu, Autorid) VALUES (4, "Rizek", 3);
 
--- Autori receptu
-INSERT INTO Autor(id, autor_jmeno, autor_prijmeni) VALUES (1, "Tomas", "Jiricek");
-INSERT INTO Autor(id, autor_jmeno, autor_prijmeni) VALUES (2, "Jan", "Jakes");
-INSERT INTO Autor(id, autor_jmeno, autor_prijmeni) VALUES (3, "Zdenek", "Pohlreich");
-
 -- Ingredience
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (1, ?, ?);
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (2, ?, ?);
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (3, ?, ?);
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (4, ?, ?);
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (5, ?, ?);
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (6, ?, ?);
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (7, ?, ?);
-INSERT INTO Ingredience(id, název_ingredience, Receptid) VALUES (?, ?, ?);
+INSERT INTO Ingredience(id, název_ingredience) VALUES (1, "Chleba");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (2, "Cesnek");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (3, "Olej");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (4, "Sul");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (5, "Mouka");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (6, "Mleko");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (7, "Cukr");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (8, "Maso");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (9, "Pepr");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (10, "Worcester");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (11, "Horcice");
+INSERT INTO Ingredience(id, název_ingredience) VALUES (12, "Strouhanka");
 
+-- Recept a Ingredience
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (1, 2, 1, 1);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (2, 2, 1, 2);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (3, 2, 1, 3);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (4, 2, 1, 4);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (5, 2, 2, 3);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (6, 1, 2, 4);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (7, 1, 2, 5);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (8, 1, 2, 6);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (9, 1, 2, 7);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (10, 1, 3, 2);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (11, 1, 3, 4);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (12, 3, 3, 8);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (13, 3, 3, 9);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (14, 3, 3, 10);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (15, 3, 3, 11);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (16, 3, 4, 3);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (17, 4, 4, 4);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (18, 4, 4, 8);
+INSERT INTO Recept_Ingredience(id, potrebaKusu, Receptid, Ingredienceid) VALUES (19, 4, 4, 12);
 
+-- Surovina
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (1, 0, "2014-10-12", 1);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (2, 1, "2014-10-15", 2);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (3, 2, "2014-10-12", 3);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (4, 3, "2014-11-16", 4);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (5, 0, "2014-11-16", 5);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (6, 0, "2014-10-15", 6);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (7, 1, "2014-11-16", 7);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (8, 1, "2014-10-15", 8);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (9, 2, "2014-12-17", 9);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (10, 0, "2014-10-12", 10);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (11, 1, "2014-10-15", 11);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (12, 2, "2014-10-12", 12);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (13, 3, "2014-11-16", 1);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (14, 0, "2014-11-16", 2);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (15, 0, "2014-10-15", 3);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (16, 1, "2014-11-16", 4);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (17, 1, "2014-10-15", 5);
+INSERT INTO Surovina(id, kusuVLednici, trvanlivost, Ingredienceid) VALUES (18, 2, "2014-12-17", 6);
 
+-- Surovina a prodejna
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (1, 1, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (2, 2, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (3, 3, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (4, 4, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (5, 5, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (6, 6, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (7, 7, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (8, 8, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (9, 9, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (10, 10, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (11, 11, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (12, 12, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (13, 13, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (14, 14, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (15, 15, 1);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (16, 16, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (17, 17, 2);
+INSERT INTO Surovina_Prodejna(id, Surovinaid, Prodejnaid) VALUES (18, 18, 1);
 
